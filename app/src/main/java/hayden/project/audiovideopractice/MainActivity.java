0203cfcb080266878eb8f169783b765a89b21d40
Hayden Bazor <hbazor@murraystate.edu>
2020-06-03 09:05:03 -0500

    Added MediaPLayer and mp3 file to playmusic and pause method for pause button

--- app/src/main/java/hayden/project/audiovideopractice/MainActivity.java
+++ app/src/main/java/hayden/project/audiovideopractice/MainActivity.java
@@ -2,6 +2,7 @@ package hayden.project.audiovideopractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +10,13 @@ import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-import java.net.URI;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private VideoView myVideoView;
     private Button myVideoButton, playMusicBtn, pauseMusicBtn;
     private MediaController mediaController;
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +25,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         myVideoView = findViewById(R.id.myVideoView);
         playMusicBtn = findViewById(R.id.playMusicBtn);
         pauseMusicBtn = findViewById(R.id.pauseMusicBtn);
-        myVideoButton = findViewById(R.id.button);
+        myVideoButton = findViewById(R.id.videoButton);
         myVideoButton.setOnClickListener(MainActivity.this);
         playMusicBtn.setOnClickListener(MainActivity.this);
         pauseMusicBtn.setOnClickListener(MainActivity.this);
+
+        mediaPlayer = new MediaPlayer();
 
         mediaController = new MediaController(MainActivity.this);
 
@@ -36,16 +39,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View buttonView) {
 
-        Uri videoUri = Uri.parse("android.resource://" + getPackageName()
-                + "/" + R.raw.video1);
+        switch (buttonView.getId()){
+            case R.id.videoButton:
+                Uri videoUri = Uri.parse("android.resource://" + getPackageName()
+                        + "/" + R.raw.video1);
 
-        //setupVideoplayer with MediaController
-        myVideoView.setVideoURI(videoUri);
-        myVideoView.setMediaController(mediaController);
-        mediaController.setAnchorView(myVideoView);
-        myVideoView.start();
+                //setupVideoPlayer with MediaController
+                myVideoView.setVideoURI(videoUri);
+                myVideoView.setMediaController(mediaController);
+                mediaController.setAnchorView(myVideoView);
+                myVideoView.start();
+                break;
+            case R.id.playMusicBtn:
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ukulele);
+                break;
+            case R.id.pauseMusicBtn:
+                onPause();
+                break;
+        }
+
 
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
 }

81ad8c0215d33199e96ca82dd3b74dbb1541beed
Hayden Bazor <hbazor@murraystate.edu>
2020-06-03 09:39:32 -0500

    Created AudioManager along with seekBar to control the volume of music.

--- app/src/main/java/hayden/project/audiovideopractice/MainActivity.java
+++ app/src/main/java/hayden/project/audiovideopractice/MainActivity.java
@@ -7,10 +7,13 @@ import android.media.MediaPlayer;
 import android.media.VolumeAutomation;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private MediaController mediaController;
     private MediaPlayer mediaPlayer;
     private AudioManager audioManager;
+    private TextView volumeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +37,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playMusicBtn = findViewById(R.id.playMusicBtn);
         pauseMusicBtn = findViewById(R.id.pauseMusicBtn);
         myVideoButton = findViewById(R.id.videoButton);
+        volumeText = findViewById(R.id.volumeText);
         seekBar = findViewById(R.id.seekBar);
         myVideoButton.setOnClickListener(MainActivity.this);
         playMusicBtn.setOnClickListener(MainActivity.this);
         pauseMusicBtn.setOnClickListener(MainActivity.this);
 
+        mediaController = new MediaController(MainActivity.this);
         mediaPlayer = new MediaPlayer();
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ukulele);
         audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);//cast to audioManager
 
+        //setting up seekbar for volume control
+        int maximumVolumeOfUserDevice = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolumeOfUserDevice = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekBar.setMax(maximumVolumeOfUserDevice);
+        seekBar.setProgress(currentVolumeOfUserDevice);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                   Toast.makeText(MainActivity.this,
+                           Integer.toString(progress),
+                           Toast.LENGTH_SHORT).show();
+                   volumeText.setText(progress);
+                   audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
     }

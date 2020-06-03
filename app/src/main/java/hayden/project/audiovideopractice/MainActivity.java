68cea6b4853080e7c6777c36cd463ae14f75b8e0
Hayden Bazor <hbazor@murraystate.edu>
2020-06-03 09:18:28 -0500

    Created AudioManager along with seekBar to control the volume of music.

--- app/src/main/java/hayden/project/audiovideopractice/MainActivity.java
+++ app/src/main/java/hayden/project/audiovideopractice/MainActivity.java
@@ -2,37 +2,46 @@ package hayden.project.audiovideopractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.VolumeAutomation;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //UI Components
+    private SeekBar seekBar;
     private VideoView myVideoView;
     private Button myVideoButton, playMusicBtn, pauseMusicBtn;
     private MediaController mediaController;
     private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initializations
         myVideoView = findViewById(R.id.myVideoView);
         playMusicBtn = findViewById(R.id.playMusicBtn);
         pauseMusicBtn = findViewById(R.id.pauseMusicBtn);
         myVideoButton = findViewById(R.id.videoButton);
+        seekBar = findViewById(R.id.seekBar);
         myVideoButton.setOnClickListener(MainActivity.this);
         playMusicBtn.setOnClickListener(MainActivity.this);
         pauseMusicBtn.setOnClickListener(MainActivity.this);
 
         mediaPlayer = new MediaPlayer();
 
-        mediaController = new MediaController(MainActivity.this);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ukulele);
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);//cast to audioManager
 
 
 
@@ -53,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 myVideoView.start();
                 break;
             case R.id.playMusicBtn:
-                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ukulele);
+                mediaPlayer.start();
                 break;
             case R.id.pauseMusicBtn:
                 onPause();
@@ -69,4 +78,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mediaPlayer.release();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mediaPlayer.start();
+    }
 }
